d7061dc82a51761a9db2b0bfa3b14b3845c12a72
Alex Autem <autem.alex@gmail.com>
2017-01-09 10:33:41 -0500

    Create new file to run game

---
+++ game.js
@@ -0,0 +1,149 @@
+var game = {
+
+  initialize: function() {
+    console.log('initializing...');
+    // Cache jQuery selectors
+    game.$hangman = $('.hangman');
+    game.$letters = $('.letters');
+    game.$guesses = $('.guesses');
+    game.$buttons = $('.buttons');
+    game.bodyParts = [$('<div id="head"></div>'),
+                     $('<div id="torso"></div>'),
+                     $('<div id="left-arm"></div>'),
+                     $('<div id="right-arm"></div>'),
+                     $('<div id="left-leg"></div>'),
+                     $('<div id="right-leg"></div>')];
+
+    // Add listeners
+    game.$buttons.on('click', '.new-game', function() {
+      game.resetGame();
+      game.addButton('guess', 'guess letter');
+      game.setHiddenPhrase(prompt('What word will the hangman hide?', 'Type word or phrase here'));
+      game.renderEmptyBoxes(game.hiddenPhrase);
+      game.setLetterIndexes(game.hiddenPhrase);
+    });
+
+    game.$buttons.on('click', '.guess', function() {
+      game.setCurrentGuess(prompt("What letter would you like to guess?", "Type letter here"));
+      game.checkGuess(game.currentGuess);
+    });
+  },
+
+  resetGame: function() {
+    console.log('resetting game board...');
+    // Remove old nodes and game data
+    game.$hangman.children().remove();
+    game.$letters.children().remove();
+    game.$guesses.text('');
+    game.hiddenPhrase = null;
+    game.letterIndexes = {};
+    game.currentGuess = null;
+    game.guesses = {};
+    game.misses = 0;
+    $('.guess').remove();
+  },
+
+  addButton: function(className, buttonText) {
+    console.log('adding button...');
+    var newButton = $('<button class=' + className + '>' + buttonText + '</button>');
+    game.$buttons.append(newButton);
+  },
+
+  setHiddenPhrase: function(phrase) {
+    console.log('setting hidden phrase...');
+    phrase = phrase.toUpperCase();
+    game.hiddenPhrase = phrase;
+  },
+
+  renderEmptyBoxes: function(phrase) {
+    console.log('rendering empty boxes...');
+    for (var i = 0; i < phrase.length; i ++) {
+      phrase[i] === ' ' ? game.$letters.append($('<div class="space"></div>')) : game.$letters.append($('<div class="letter"></div>'));
+    }
+    game.centerEmptyBoxes(phrase);
+  },
+
+  setLetterIndexes: function(phrase) {
+    console.log('setting up letter indexes object...');
+    for (var i = 0; i < phrase.length; i ++) {
+      if (phrase[i] !== ' ') {
+        if (game.letterIndexes[phrase[i]]) {
+          game.letterIndexes[phrase[i]].push(i);
+        } else {
+          game.letterIndexes[phrase[i]] = [i];
+        }
+      }
+    }
+  },
+
+  centerEmptyBoxes: function(phrase) {
+    console.log('centering empty boxes...');
+    if (phrase.length > 15) {
+      game.$letters.css('width', '1000px');
+    } else {
+      game.$letters.css('width', '' + (64 * phrase.length + 2) + 'px');
+    }
+  },
+
+  setCurrentGuess: function(guess) {
+    console.log('setting current guess...');
+    guess = guess.toUpperCase();
+    if (game.guesses[guess]) {
+      game.setCurrentGuess(prompt('You already guessed that letter. Guess again!', 'Type letter here'));
+    } else if (guess.length > 1) {
+      game.setCurrentGuess(prompt('One letter at a time, please. Guess again!', 'Type letter here'));
+    } else {
+      game.currentGuess = guess;
+      game.guesses[guess] = true;
+    }
+  },
+
+  checkGuess: function(guess) {
+    console.log('checking guess...');
+    if (game.letterIndexes[guess]) {
+      game.renderCorrectGuess(guess);
+    } else {
+      game.renderIncorrectGuess(guess);
+      game.renderHangmanBodyPart();
+    }
+  },
+
+  renderCorrectGuess: function(guess) {
+    console.log('rendering correct guess...');
+    var indexes = game.letterIndexes[guess];
+    indexes.forEach(function(index) {
+      game.$letters.children().eq(index).text(guess);
+    });
+    delete game.letterIndexes[guess];
+    if (Object.keys(game.letterIndexes).length === 0) {
+      game.gameOver('win');
+    }
+  },
+
+  renderIncorrectGuess: function(guess) {
+    console.log('rendering incorrect guess...');
+    var oldGuesses = game.$guesses.text();
+    game.$guesses.text(oldGuesses + ' ' + guess);
+  },
+
+  renderHangmanBodyPart: function() {
+    console.log('rendering hangman body part...');
+    game.$hangman.append(game.bodyParts[game.misses]);
+    game.misses = game.misses + 1;
+    if (game.misses === 6) {
+      game.gameOver('lose');
+    }
+  },
+
+  gameOver: function(winOrLose) {
+    console.log('game over...');
+    game.$guesses.text('');
+    if (winOrLose === 'win') {
+      game.$guesses.append($('<span class="winner">WINNER WINNER!</span>'));
+    } else {
+      game.$guesses.append($('<span class="loser">YOU LOSE. TRY AGAIN!</span>'));
+    }
+    $('.guess').remove();
+  }
+
+};
