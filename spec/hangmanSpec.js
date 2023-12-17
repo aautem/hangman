3222eba56c41324727e0b04938ba940c11cc1ead
Alex Autem <autem.alex@gmail.com>
2023-12-16 20:00:53 -0600

    Formatting

--- spec/hangmanSpec.js
+++ spec/hangmanSpec.js
@@ -1,83 +1,83 @@
-var expect = require('chai').expect;
-var game = require('./../game.js');
+var expect = require("chai").expect;
+var game = require("./../game.js");
 
-describe('Hangman', function() {
-  describe('game methods', function() {
-    it('initialize should be a function', function() {
-      expect(game.initialize).to.be.a('function');
+describe("Hangman", function () {
+  describe("game methods", function () {
+    it("initialize should be a function", function () {
+      expect(game.initialize).to.be.a("function");
     });
-    it('resetGame should be a function', function() {
-      expect(game.resetGame).to.be.a('function');
+    it("resetGame should be a function", function () {
+      expect(game.resetGame).to.be.a("function");
     });
-    it('addButton should be a function', function() {
-      expect(game.addButton).to.be.a('function');
+    it("addButton should be a function", function () {
+      expect(game.addButton).to.be.a("function");
     });
-    it('setHiddenPhrase should be a function', function() {
-      expect(game.setHiddenPhrase).to.be.a('function');
+    it("setHiddenPhrase should be a function", function () {
+      expect(game.setHiddenPhrase).to.be.a("function");
     });
-    it('hideRandomWord should be a function', function() {
-      expect(game.hideRandomWord).to.be.a('function');
+    it("hideRandomWord should be a function", function () {
+      expect(game.hideRandomWord).to.be.a("function");
     });
-    it('renderEmptyBoxes should be a function', function() {
-      expect(game.renderEmptyBoxes).to.be.a('function');
+    it("renderEmptyBoxes should be a function", function () {
+      expect(game.renderEmptyBoxes).to.be.a("function");
     });
-    it('setLetterIndexes should be a function', function() {
-      expect(game.setLetterIndexes).to.be.a('function');
+    it("setLetterIndexes should be a function", function () {
+      expect(game.setLetterIndexes).to.be.a("function");
     });
-    it('centerEmptyBoxes should be a function', function() {
-      expect(game.centerEmptyBoxes).to.be.a('function');
+    it("centerEmptyBoxes should be a function", function () {
+      expect(game.centerEmptyBoxes).to.be.a("function");
     });
-    it('setCurrentGuess should be a function', function() {
-      expect(game.setCurrentGuess).to.be.a('function');
+    it("setCurrentGuess should be a function", function () {
+      expect(game.setCurrentGuess).to.be.a("function");
     });
-    it('checkGuess should be a function', function() {
-      expect(game.checkGuess).to.be.a('function');
+    it("checkGuess should be a function", function () {
+      expect(game.checkGuess).to.be.a("function");
     });
-    it('renderCorrectGuess should be a function', function() {
-      expect(game.renderCorrectGuess).to.be.a('function');
+    it("renderCorrectGuess should be a function", function () {
+      expect(game.renderCorrectGuess).to.be.a("function");
     });
-    it('renderIncorrectGuess should be a function', function() {
-      expect(game.renderIncorrectGuess).to.be.a('function');
+    it("renderIncorrectGuess should be a function", function () {
+      expect(game.renderIncorrectGuess).to.be.a("function");
     });
-    it('renderHangmanBodyPart should be a function', function() {
-      expect(game.renderHangmanBodyPart).to.be.a('function');
+    it("renderHangmanBodyPart should be a function", function () {
+      expect(game.renderHangmanBodyPart).to.be.a("function");
     });
-    it('gameOver should be a function', function() {
-      expect(game.gameOver).to.be.a('function');
+    it("gameOver should be a function", function () {
+      expect(game.gameOver).to.be.a("function");
     });
   });
 
-  describe('hideRandomWord', function() {
+  describe("hideRandomWord", function () {
     // hide a random word
     game.hideRandomWord();
 
-    it('should have a hiddenPhrase property', function() {
-      expect(game).to.have.property('hiddenPhrase');
+    it("should have a hiddenPhrase property", function () {
+      expect(game).to.have.property("hiddenPhrase");
     });
-    it('hiddenPhrase should be a string', function() {
-      expect(game.hiddenPhrase).to.be.a('string');
+    it("hiddenPhrase should be a string", function () {
+      expect(game.hiddenPhrase).to.be.a("string");
     });
   });
 
-  describe('setHiddenPhrase', function() {
+  describe("setHiddenPhrase", function () {
     // set hiddenPhrase to HANGMAN
-    game.setHiddenPhrase('hangman');
+    game.setHiddenPhrase("hangman");
 
-    it('should set the hiddenPhrase value', function() {
-      expect(game.hiddenPhrase).to.equal('HANGMAN');
+    it("should set the hiddenPhrase value", function () {
+      expect(game.hiddenPhrase).to.equal("HANGMAN");
     });
   });
 
-  describe('setCurrentGuess', function() {
+  describe("setCurrentGuess", function () {
     // set letterIndexes of HANGMAN
     game.guesses = {};
-    game.setCurrentGuess('a');
+    game.setCurrentGuess("a");
 
-    it('should create a currentGuess property', function() {
-      expect(game).to.have.property('currentGuess');
+    it("should create a currentGuess property", function () {
+      expect(game).to.have.property("currentGuess");
     });
-    it('should create a guesses property', function() {
-      expect(game).to.have.property('guesses');
+    it("should create a guesses property", function () {
+      expect(game).to.have.property("guesses");
     });
   });
 });
